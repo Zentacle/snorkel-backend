23714d239e83531889469c894ed037e38d7ba9ce
Mayank Jain <mjmayank@gmail.com>
2021-07-28 00:07:59 -0700

    Fix bug with images

--- app/__init__.py
+++ app/__init__.py
@@ -325,7 +325,7 @@ def add_review():
   text = request.json.get('text')
   rating = request.json.get('rating')
   activity_type = request.json.get('activity_type')
-  imageURLs = request.json.get('images')
+  imageURLs = request.json.get('images') or []
   date_dived = dateutil.parser.isoparse(request.json.get('date_dived')) if request.json.get('date_dived') else datetime.utcnow()
   if not rating:
     return { 'msg': 'Please select a rating' }, 401
