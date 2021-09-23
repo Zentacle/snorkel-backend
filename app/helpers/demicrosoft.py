8e5df15051230523c6521b8163108b37f81bbd1d
Mayank Jain <mjmayank@gmail.com>
2021-09-23 10:33:27 -1000

    Add dash as acceptable in demicrosoft function

--- app/helpers/demicrosoft.py
+++ app/helpers/demicrosoft.py
@@ -1,7 +1,7 @@
 import re
 
 def demicrosoft(fn):
-    fn = re.sub("[^0-9a-zA-Z ]+", "", fn)
+    fn = re.sub("[^0-9a-zA-Z -]+", "", fn)
     for ch in [' ']:
         fn = fn.replace(ch,"-")
     return fn
