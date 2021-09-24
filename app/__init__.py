62a55b47e984fc5562293743e1b3a4b749358197
Mayank Jain <mjmayank@gmail.com>
2021-09-23 21:22:04 -1000

    Remove unnecessary if statement in get spots endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -375,37 +375,36 @@ def get_spots():
     query = query.filter(Spot.is_verified.isnot(True))
   else:
     query = query.filter(Spot.is_verified.isnot(False))
-  if request.args.get('limit') != 'none':
-    locality_name = request.args.get('locality')
-    area_two_name = request.args.get('area_two')
-    area_one_name = request.args.get('area_one')
-    country_name = request.args.get('country')
-    if locality_name:
-      query = query.filter(Spot.locality.has(short_name=locality_name))
-      area = Locality.query \
-        .options(joinedload('area_two')) \
-        .options(joinedload('area_one')) \
+  locality_name = request.args.get('locality')
+  area_two_name = request.args.get('area_two')
+  area_one_name = request.args.get('area_one')
+  country_name = request.args.get('country')
+  if locality_name:
+    query = query.filter(Spot.locality.has(short_name=locality_name))
+    area = Locality.query \
+      .options(joinedload('area_two')) \
+      .options(joinedload('area_one')) \
+      .options(joinedload('country')) \
+      .filter_by(short_name=locality_name) \
+      .first_or_404()
+  elif area_two_name:
+    query = query.filter(Spot.area_two.has(short_name=area_two_name))
+    area = AreaTwo.query \
+      .options(joinedload('area_one')) \
+      .options(joinedload('country')) \
+      .filter_by(short_name=area_two_name) \
+      .first_or_404()
+  elif area_one_name:
+      query = query.filter(Spot.area_one.has(short_name=area_one_name))
+      area = AreaOne.query \
         .options(joinedload('country')) \
-        .filter_by(short_name=locality_name) \
+        .filter_by(short_name=area_one_name) \
         .first_or_404()
-    elif area_two_name:
-      query = query.filter(Spot.area_two.has(short_name=area_two_name))
-      area = AreaTwo.query \
-        .options(joinedload('area_one')) \
-        .options(joinedload('country')) \
-        .filter_by(short_name=area_two_name) \
+  elif country_name:
+      query = query.filter(Spot.country.has(short_name=country_name))
+      area = Country.query \
+        .filter_by(short_name=country_name) \
         .first_or_404()
-    elif area_one_name:
-        query = query.filter(Spot.area_one.has(short_name=area_one_name))
-        area = AreaOne.query \
-          .options(joinedload('country')) \
-          .filter_by(short_name=area_one_name) \
-          .first_or_404()
-    elif country_name:
-        query = query.filter(Spot.country.has(short_name=country_name))
-        area = Country.query \
-          .filter_by(short_name=country_name) \
-          .first_or_404()
   sort_param = request.args.get('sort')
   if sort_param == 'latest':
     query = query.order_by(Spot.last_review_date.desc().nullslast())
