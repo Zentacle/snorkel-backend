f3e3a0012d86e365cc158eb656d76f1df87e63ac
Mayank Jain <mjmayank@gmail.com>
2022-05-06 20:42:32 -0400

    Update description from the backend for blank descriptions

--- app/models.py
+++ app/models.py
@@ -190,6 +190,10 @@ class Spot(db.Model):
             data.pop('is_verified', None)
         if data.get('is_deleted'):
             data.pop('is_deleted', None)
+        if not data.get('description').strip():
+            data['description'] = f'''{data.get('name')} is a {data.get('rating') if data.get('rating') else 0}-star
+                rated scuba dive and snorkel destination in {data.get('location_city')} which is accessible from
+                shore based on {data.get('num_reviews')} ratings.'''
         if data.get('tags'):
             data['access'] = []
             for tag in data.get('tags'):
