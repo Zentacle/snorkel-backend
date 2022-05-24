db6f568f53d7f9e234adc225292bef912e3b8129
Nnanyielugo <kz.onyegbule@gmail.com>
2022-05-24 12:04:30 +0100

    first steps towards modularity

--- app/__init__.py
+++ app/__init__.py
@@ -3142,52 +3142,8 @@ def reset_password():
     return resp
   return {'msg': 'No token or password provided'}, 422
 
-@app.route('/dive_shops/get', methods=['GET'])
-def fetch_dive_shops():
-  dive_shops = DiveShop.query.all()
-  data = [dive_shop.get_simple_dict() for dive_shop in dive_shops]
-  return { 'data': data }
-
-@app.route('/dive_shops/create', methods=['POST'])
-@jwt_required()
-def create_dive_shop():
-  url = request.json.get('url')
-  fareharbor_url = request.json.get('fareharbor_url')
-  address1 = request.json.get('address1')
-  address2 = request.json.get('address2')
-  city = request.json.get('city')
-  state = request.json.get('state')
-  zip = request.json.get('zip')
-  logo_img = request.json.get('logo_img')
-  latitude = request.json.get('latitude')
-  longitude = request.json.get('longitude')
-  locality_id = request.json.get('locality_id')
-  area_one_id = request.json.get('area_one_id')
-  area_two_id  = request.json.get('area_two_id')
-  country_id = request.json.get('country_id')
-
-  dive_shop = DiveShop(
-    url=url,
-    fareharbor_url=fareharbor_url,
-    address1=address1,
-    address2=address2,
-    city=city,
-    state=state,
-    zip=zip,
-    logo_img=logo_img,
-    latitude=latitude,
-    longitude=longitude,
-    locality_id=locality_id,
-    area_one_id=area_one_id,
-    area_two_id=area_two_id,
-    country_id=country_id
-  )
-
-  db.session.add(dive_shop)
-  db.session.commit()
-
-  return { 'dive_shop': dive_shop.get_dict() }
-
+from app.routes import dive_shop
+app.register_blueprint(dive_shop.bp)
 
 @app.route('/spot/tide')
 @cache.cached(query_string=True, timeout=3600)
---
+++ app/routes/dive_shop.py
@@ -0,0 +1,56 @@
+from flask import Blueprint, request
+from app.models import DiveShop
+from app import db
+from flask_jwt_extended import jwt_required
+
+bp = Blueprint('dive_shop', __name__, url_prefix="/dive_shops")
+
+@bp.route('/get', methods=['GET'])
+def fetch_dive_shops():
+  dive_shops = DiveShop.query.all()
+  data = [dive_shop.get_simple_dict() for dive_shop in dive_shops]
+  return { 'data': data }
+
+@bp.route('/get/<int:id>', methods=['GET'])
+def fetch_dive_shop(id):
+  print('id selected', id)
+
+@bp.route('/create', methods=['POST'])
+@jwt_required()
+def create_dive_shop():
+  url = request.json.get('url')
+  fareharbor_url = request.json.get('fareharbor_url')
+  address1 = request.json.get('address1')
+  address2 = request.json.get('address2')
+  city = request.json.get('city')
+  state = request.json.get('state')
+  zip = request.json.get('zip')
+  logo_img = request.json.get('logo_img')
+  latitude = request.json.get('latitude')
+  longitude = request.json.get('longitude')
+  locality_id = request.json.get('locality_id')
+  area_one_id = request.json.get('area_one_id')
+  area_two_id  = request.json.get('area_two_id')
+  country_id = request.json.get('country_id')
+
+  dive_shop = DiveShop(
+    url=url,
+    fareharbor_url=fareharbor_url,
+    address1=address1,
+    address2=address2,
+    city=city,
+    state=state,
+    zip=zip,
+    logo_img=logo_img,
+    latitude=latitude,
+    longitude=longitude,
+    locality_id=locality_id,
+    area_one_id=area_one_id,
+    area_two_id=area_two_id,
+    country_id=country_id
+  )
+
+  db.session.add(dive_shop)
+  db.session.commit()
+
+  return { 'dive_shop': dive_shop.get_dict() }
