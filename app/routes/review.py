4c8a3ddf7a52943d04dec61dbed5f4852621d174
Nnanyielugo <kz.onyegbule@gmail.com>
2022-06-11 22:32:37 +0100

    initialize dive shop to an empty object so that we don't call get_dict on possible null object

--- app/routes/review.py
+++ app/routes/review.py
@@ -243,9 +243,11 @@ def get_review():
     review = Review.query.filter_by(id=review_id).first()
     spot = review.spot
     data = review.get_dict()
-    dive_shop = review.dive_shop
+    dive_shop = {}
+    if review.dive_shop:
+      dive_shop = review.dive_shop.get_dict()
     data['user'] = review.user.get_dict()
-    return { 'review': data, 'spot': spot.get_dict(), 'dive_shop': dive_shop.get_dict() }
+    return { 'review': data, 'spot': spot.get_dict(), 'dive_shop': dive_shop }
   else:
     sd_id = request.args.get('sd_review_id')
     if sd_id:
