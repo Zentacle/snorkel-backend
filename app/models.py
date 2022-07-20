efdc40c842e9473984b0571e1d01a038874b46d6
Mayank Jain <mjmayank@gmail.com>
2022-07-20 17:58:24 -0400

    Ensure PADI store IDs are unique

--- app/models.py
+++ app/models.py
@@ -556,7 +556,7 @@ class WannaDiveData(db.Model):
 
 class DiveShop(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    padi_store_id = db.Column(db.String)
+    padi_store_id = db.Column(db.String, unique=True)
     name = db.Column(db.String)
     description = db.Column(db.String)
     hours = db.Column(db.JSON(none_as_null=True))
