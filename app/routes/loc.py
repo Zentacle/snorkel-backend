0a89f17424fc53cdb11f97b60c98f307f6c8a7d3
Mayank Jain <mjmayank@gmail.com>
2022-07-24 15:18:17 -0400

    Add patch for locality endpoint

--- app/routes/loc.py
+++ app/routes/loc.py
@@ -3,6 +3,7 @@ from app.models import (
   Country,
   AreaOne,
   AreaTwo,
+  Locality,
 )
 from app import db
 
@@ -42,6 +43,21 @@ def patch_loc_one():
 def patch_loc_two():
   id = request.json.get('id')
   loc = AreaTwo.query.filter_by(id=id).first_or_404()
+  updates = request.json
+  updates.pop('id', None)
+  try:
+    for key in updates.keys():
+      setattr(loc, key, updates.get(key))
+  except ValueError as e:
+    return e, 500
+  db.session.commit()
+  loc.id
+  return loc.get_dict(), 200
+
+@bp.route("/locality/patch", methods=["PATCH"])
+def patch_locality():
+  id = request.json.get('id')
+  loc = Locality.query.filter_by(id=id).first_or_404()
   updates = request.json
   updates.pop('id', None)
   try:
