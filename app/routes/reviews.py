4fc87e344a2fb555a4b322f1a27322bd424d5b53
Mayank Jain <mjmayank@gmail.com>
2022-08-07 20:22:01 -0400

    Generate weekly email

--- app/routes/reviews.py
+++ app/routes/reviews.py
@@ -120,128 +120,60 @@ def delete_shore_diving_ids():
 
 @bp.route("/recentemail")
 def send_recent_reviews():
-  name='Mala Wharf'
-  city_location='Lahaina, Maui'
-  profile_pic='https://snorkel.s3.amazonaws.com/profile_pic/mjmayank.jpeg'
-  display_name='Mayank Jain'
-  formatted_date='March 2nd'
-  hero_img='https://snorkel.s3.amazonaws.com/hero/puako_village_end.jpeg'
-  text='This is my favorite scuba spot on the whole island. You are guaranteed to ...'
-  beach_url='https://www.zentacle.com/Beach/1/mala-wharf'
-  css = '''
-  <style>
-    .container {
-      padding: 8px;
-      margin-bottom: 8px;
-    }
-
-    .title {
-      font-size: 20px;
-      font-weight: 500;
-      margin-bottom: 8px;
-    }
-
-    .metadataContainer {
-      display: flex;
-      margin-bottom: 8px;
-      align-items: center;
-    }
-
-    .profilePicContainer {
-      height: 16px;
-      width: 16px;
-    }
-
-    .profilePicContainer {
-      height: 16px;
-      width: 16px;
-      border-radius: 100%;
-      overflow: hidden;
-      margin-right: 2px;
-    }
-
-    .profilePic {
-      height: 100%;
-      width: 100%;
-    }
-
-    .stars {
-      height: 16px;
-      width: auto;
-      margin: 0 4px;
-    }
-
-    .reviewContainer {
-      display: flex;
-    }
-
-    .reviewTextContainer {
-      display: flex;
-      flex-direction: column;
-      justify-content: space-between;
-    }
-
-    .reviewText {
-      flex: 0 0 40px;
-      overflow: hidden;
-      text-overflow: ellipsis;
-    }
-
-    .heroImgContainer {
-      flex: 0 0 80px;
-      height: 78px;
-      width: 78px;
-      margin-right: 8px;
-      overflow: hidden;
-      border-radius: 8px;
-    }
-
-    .heroImg {
-      height: 100%;
-      width: 100%;
-    }
-
-    .seeMoreButton {
-      background-color: #0087D3;
-      border-radius: 32px;
-      color: white;
-      padding: 8px 4px;
-      text-align: center;
-      text-decoration: none;
-    }
-  </style>
+  reviews = Review.query \
+    .options(joinedload('user')) \
+    .options(joinedload('spot')) \
+    .order_by(Review.date_posted.desc()).limit(10).all()
+  one_star = '''
+    <img style='height: 16px;width: auto;margin: 0 4px;' src='https://www.zentacle.com/_next/image?url=%2Ffullstar.png&w=64&q=75' />
   '''
-  html = f'''
-    <div class='container'>
-    <div class='title'>{name} in {city_location}</div>
-    <div class='metadataContainer'>
-      <div class='profilePicContainer'>
-        <img class='profilePic' src='{profile_pic}' />
+  stars = ''
+  for x in range(5):
+    stars += one_star
+  first_name='Mayank'
+  intro = f'''
+    <div style='padding: 0 8px'>Hi {first_name},</div>
+    <div style='padding: 0 8px'>Check out the most recent reviews and dive logs posted on Zentacle!</div>
+  '''
+  html = intro
+  for review in reviews:
+    name=review.spot.name
+    city_location=review.spot.location_city
+    profile_pic=review.user.profile_pic
+    display_name=review.user.display_name
+    formatted_date='August 7th'
+    hero_img=review.spot.hero_img
+    text=review.text
+    beach_url=f'https://www.zentacle.com{review.spot.get_url()}'
+    post = f'''
+      <div style='padding: 8px;margin-bottom: 8px;'>
+      <a href='{beach_url}' style='font-size: 20px;font-weight: 500;margin-bottom: 8px;text-decoration:none'>{name} in {city_location}</a>
+      <div style='display: flex;margin-bottom: 8px;align-items: center;'>
+        <div style='height: 16px;width: 16px;border-radius: 100%;overflow: hidden;margin-right: 2px;'>
+          <img style='height: 100%;width: 100%;' src='{profile_pic}' />
+        </div>
+        <div>
+          {display_name} -
+        </div>
+        {stars}
+        <div>
+          on {formatted_date}
+        </div>
       </div>
-      <div>
-        {display_name} -
-      </div>
-      <img class='stars' src='https://www.zentacle.com/_next/image?url=%2Ffullstar.png&w=64&q=75' />
-      <div>
-        on {formatted_date}
-      </div>
-    </div>
-    <div class='reviewContainer'>
-      <div class='heroImgContainer'>
-        <img class='heroImg' src='{hero_img}' />
-      </div>
-      <div class='reviewTextContainer'>
-        <div class='reviewText'>{text}</div>
-        <a class='seeMoreButton' href='{beach_url}'>
-          See more
+      <div style='display: flex;'>
+        <a href='{beach_url}' style='flex: 0 0 80px;height: 78px;width: 78px;margin-right: 8px;overflow: hidden;border-radius: 8px;'>
+          <img style='height: 100%;width: 100%;' src='{hero_img}' />
         </a>
+        <div>
+          <div style='flex: 0 0 40px;overflow: hidden;text-overflow: ellipsis;margin-bottom: 8px;'>{text}</div>
+          <a style='background-color: #0087D3;border-radius: 32px;color: white;padding: 8px 16px;text-align: center;text-decoration: none;' href='{beach_url}'>
+            See more
+          </a>
+        </div>
       </div>
     </div>
-  </div>
-  '''
-  for x in range(3):
-    html += html
-  html += css
+    '''
+    html += post
   message = Mail(
     from_email=('hello@zentacle.com', 'Zentacle'),
     to_emails='mayank@zentacle.com',
@@ -252,5 +184,5 @@ def send_recent_reviews():
   sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
   print(html)
   return 'ok'
-  response = sg.send(message)
+  # response = sg.send(message)
   return 'ok'
